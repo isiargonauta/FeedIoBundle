4c020608740de9ba4af17e27820f632f156aa9e0
Alex Debril <alex.debril@gmail.com>
2015-02-22 23:51:26 +0100

    Use services.yml

--- DependencyInjection/DebrilFeedIoExtension.php
+++ DependencyInjection/DebrilFeedIoExtension.php
@@ -25,8 +25,8 @@ class DebrilFeedIoExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yml');
 
         $default = array(
             \DateTime::RFC3339,
