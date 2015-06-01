47526f8bcebf8ac17981aeabeec85a2b7dd9d71a
Alex Debril <alex.debril@gmail.com>
2015-06-01 09:10:53 +0200

    strings replacement

--- Adapter/MockStorage.php
+++ Adapter/MockStorage.php
@@ -38,15 +38,15 @@ class MockStorage implements StorageInterface
 
         $feed->setPublicId($id);
 
-        $feed->setTitle('thank you for using RssAtomBundle');
+        $feed->setTitle('thank you for using FeedIoBundle');
         $feed->setDescription('this is the mock FeedContent');
-        $feed->setLink('https://raw.github.com/alexdebril/rss-atom-bundle/');
+        $feed->setLink('https://raw.github.com/alexdebril/FeedIoBundle/');
         $feed->setLastModified(new \DateTime());
 
         $item = new Item();
 
         $item->setPublicId('1');
-        $item->setLink('https://raw.github.com/alexdebril/rss-atom-bundle/somelink');
+        $item->setLink('https://raw.github.com/alexdebril/FeedIoBundle/somelink');
         $item->setTitle('This is an item');
         $item->setDescription('this stream was generated using the MockProvider class');
         $item->setLastModified(new \DateTime());
