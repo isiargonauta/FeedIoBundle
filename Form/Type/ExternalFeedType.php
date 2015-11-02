b9542251d36935367dbceec53f25fff4c21c7f36
Alex Debril <alex.debril@gmail.com>
2015-11-02 19:15:05 +0100

    external feed form

---
+++ Form/Type/ExternalFeedType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Debril\FeedIoBundle\Form\Type;
+
+use Debril\FeedIoBundle\Entity\Feed;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ExternalFeedType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $feed = new Feed;
+        $builder
+            ->add('link')
+            ->add('submit', 'submit', array('label' => 'Add'))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Debril\FeedIoBundle\Entity\Feed'
+        ));
+    }
+    
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'debril_feediobundle_feed_external';
+    }
+}
