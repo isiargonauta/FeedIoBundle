8eda4d468426a1e6b8fe92ab76dccaead5f50fe4
Alex Debril <alex.debril@gmail.com>
2015-11-10 08:43:11 +0100

    published feeds form

---
+++ Form/Type/PublishedFeedType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Debril\FeedIoBundle\Form\Type;
+
+use Debril\FeedIoBundle\Entity\Feed;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PublishedFeedType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $feed = new Feed();
+        $builder           
+            ->add('type', 'choice', array('choices' => $feed->getAvailableTypes()))
+            ->add('comment')
+            ->add('publicId')
+            ->add('description')
+            ->add('title')
+            ->add('submit', 'submit', array('label' => 'Save'))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Debril\FeedIoBundle\Entity\Feed'
+        ));
+    }
+    
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'debril_feediobundle_feed_published';
+    }
+}
