e13b191b9136c244e0e37034fbd766c2f0a0debc
Alex Debril <alex.debril@gmail.com>
2015-11-14 18:37:44 +0100

    list unit test

--- Tests/Controller/ItemControllerTest.php
+++ Tests/Controller/ItemControllerTest.php
@@ -2,22 +2,33 @@
 
 namespace Debril\FeedIoBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class ItemControllerTest extends WebTestCase
+class ItemControllerTest extends WebDbTestCase
 {
+
+    public function setUp()
+    {
+        parent::setUp();
+        self::runCommand('doctrine:fixtures:load -n');
+    }
+
     public function testList()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/list');
+        $crawler = $client->request('GET', '/item/list/1');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /item/list/1");
+        $content = $client->getResponse()->getContent();
+        $this->assertGreaterThan(0, strpos($content, '<h1>PHP : Hypertext Preprocessor</h1>'), 'Missing feed title');
+        $this->assertGreaterThan(0, strpos($content, '<h2>test</h2>'), 'Missing item title');
+
     }
 
     public function testNew()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/new');
+        $crawler = $client->request('GET', '/item/new/1');
+
     }
 
     public function testEdit()
