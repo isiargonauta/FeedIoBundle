f09650d7dabd3fd31b8642907e3a077846fdce59
Alex Debril <alex.debril@gmail.com>
2015-11-14 01:09:24 +0100

    ItemController tests

---
+++ Tests/Controller/ItemControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Debril\FeedIoBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ItemControllerTest extends WebTestCase
+{
+    public function testList()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/list');
+    }
+
+    public function testNew()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/new');
+    }
+
+    public function testEdit()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/edit');
+    }
+
+    public function testDelete()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/delete');
+    }
+
+}
