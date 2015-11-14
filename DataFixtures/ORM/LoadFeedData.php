09ec1ab9a51dcf053b110a922cac38fc06b6ac8b
Alex Debril <alex.debril@gmail.com>
2015-11-14 18:20:31 +0100

    Added an item to the feed

--- DataFixtures/ORM/LoadFeedData.php
+++ DataFixtures/ORM/LoadFeedData.php
@@ -4,7 +4,8 @@ namespace Debril\FeedIoBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface; 
 use Doctrine\Common\Persistence\ObjectManager; 
-use Debril\FeedIoBundle\Entity\Feed; 
+use Debril\FeedIoBundle\Entity\Feed;
+use Debril\FeedIoBundle\Entity\Item;
 
 class LoadFeedData implements FixtureInterface 
 
@@ -19,7 +20,14 @@ class LoadFeedData implements FixtureInterface
         $feed->setTitle('PHP : Hypertext Preprocessor');
         $feed->setType(Feed::TYPE_EXTERNAL);
         $feed->setLastModified(new \DateTime());
-        
+
+        $item = new Item();
+        $item->setTitle('test');
+        $item->setDescription('description');
+        $item->setLastModified(new \DateTime());
+        $item->setLink('http://some.link');
+
+        $feed->add($item);
         $manager->persist($feed); 
         $manager->flush(); 
     } 
